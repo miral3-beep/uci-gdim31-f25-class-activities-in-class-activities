71bbce134a01274ced04941c7794adfc7dd81fa1
miral3-beep <miral3@uci.edu>
2025-10-06 19:49:21 -0700

    week 2 acivities

--- In Class Activities/Assets/W2/Scripts/Ball.cs
+++ In Class Activities/Assets/W2/Scripts/Ball.cs
@@ -21,7 +21,7 @@ public class Ball : MonoBehaviour
         //_bounces = _bounces + 2;
         //_bounces + 1
         //_bounces --;
-        //_bounces++;
+        _bounces++;
         //_bounces += 1
         // STEP 1 -------------------------------------------------------------
 
@@ -44,7 +44,7 @@ public class Ball : MonoBehaviour
         // Uncomment ONE of the below lines to ADD 0.1 to the value of 'r'.
         //r += 0.1f
         //r * 0.1;
-        //r = r + 0.1f;
+        r = r + 0.1f;
         //r = r * 0.1f;
         //r += 0.1;
         // STEP 2 -------------------------------------------------------------
@@ -57,27 +57,27 @@ public class Ball : MonoBehaviour
             // STEP 3 ---------------------------------------------------------
             // Uncomment ONE of the below lines of code to set the value of 'r' to ZERO.
             //r = 0.0f
-            //r = 0.0f;
+            r = 0.0f;
             //r 0.0f;
             // STEP 3 ---------------------------------------------------------
         }
 
         // STEP 4 -------------------------------------------------------------
         // Uncomment the below line and fix it to SUBTRACT 0.1 from the value of 'g'.
-        //g -= 0.1f
+        g -= 0.1f;
         // STEP 4 -------------------------------------------------------------
 
         if (g < 0.0f)
         {
             // STEP 5 ---------------------------------------------------------
             // Uncomment the below line and fix it to set the value of 'g' to ONE.
-            //g = 1.0;
+            g = 1.0f;
             // STEP 5 ---------------------------------------------------------
         }
 
         // STEP 6 -------------------------------------------------------------
         // Below this comment, write a line of code to MULTIPLY 'b' by 1.2.
-
+b *= 1.2f;
         // STEP 6 -------------------------------------------------------------
 
         // STEP 7 -------------------------------------------------------------
